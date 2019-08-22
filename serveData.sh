4025c968162c4463838da9a465d8bef35655b551
James <jwblangley@gmail.com>
2019-08-22 18:50:43 +0100

    Update serve script to include default manager json

--- serveData.sh
+++ serveData.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 PORT=8000
-
-# Set working directory variable to that described in directory.txt
+defaultjson=$(cat "$(dirname $0)/defaultManager.json")
 wd=$(cat directory.txt | envsubst)
 
 # If directory does not exist, create it and change to it
 cd $wd &> /dev/null || { mkdir $wd && echo "Creating directory: $wd" && cd $wd; }
 echo "Switching to directory: $wd"
+
+
+test -f "$wd/manager.json" || echo "$defaultjson" > "$wd/manager.json"
+
 
 # Serve the files
 http-server -p $PORT
