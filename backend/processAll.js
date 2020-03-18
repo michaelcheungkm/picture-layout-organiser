cfc7695c66e168ca4ca8afd5c7135bcd420f6bd7
James Langley <jwblangley@gmail.com>
2020-03-18 21:43:51 +0000

    Update processAll.js to allow command line argument inputs

--- backend/processAll.js
+++ backend/processAll.js
@@ -3,19 +3,24 @@ const imageProcessing = require('./imageProcessing.js')
 
 const NUM_HASHES = 50
 
+const args = process.argv.slice(2)
 
-const directory = './test'
-const files = fs.readdirSync(directory)
+const directory = args[0]
 
-var count = 0
+try {
+  const files = fs.readdirSync(directory)
 
-for (var i = 0; i < files.length; i++) {
-  const file = files[i]
+  var count = 0
+  for (var i = 0; i < files.length; i++) {
+    const file = files[i]
 
-  imageProcessing.processImage(directory + '/' + file, err => {
-    const outOf = ++count * NUM_HASHES / files.length
+    imageProcessing.processImage(directory + '/' + file, err => {
+      const outOf = ++count * NUM_HASHES / files.length
 
-    console.log((err ? 'Could not process ' : 'Processed: ') + file + ' '.repeat(NUM_HASHES))
-    process.stdout.write('[' + '#'.repeat(Math.ceil(outOf)) + ' '.repeat(Math.floor(NUM_HASHES - outOf)) + '] ' + Math.ceil(outOf / NUM_HASHES * 100) +'%\r')
-  }, true)
+      console.log((err ? 'Could not process ' : 'Processed: ') + file + ' '.repeat(NUM_HASHES))
+      process.stdout.write('[' + '#'.repeat(Math.ceil(outOf)) + ' '.repeat(Math.floor(NUM_HASHES - outOf)) + '] ' + Math.ceil(outOf / NUM_HASHES * 100) +'%\r')
+    }, true)
+  }
+} catch (err) {
+  console.error('Could not read directory: ' + directory)
 }
