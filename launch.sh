cf8e4f5e4bde413f2ec1b8cfb786a80a3ad00965
James Langley <jwblangley@gmail.com>
2020-05-31 12:36:30 +0000

    Replace backticks with single quotes in launch.sh

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
-npm run build --prefix frontend;
+npm run build --prefix frontend
 
-tmux new -d -s "plo-backend" "npm start --prefix backend";
-tmux new -d -s "plo-frontend" "serve -p 3000 frontend/build";
+tmux new -d -s "plo-backend" "npm start --prefix backend"
+tmux new -d -s "plo-frontend" "serve -p 3000 frontend/build"
 
-echo "Application launched";
-echo "Run `tmux a -t plo-backend` and `tmux a -t plo-frontend` to reattach the detached terminals"
+echo "Application launched"
+echo "Run 'tmux a -t plo-backend' and 'tmux a -t plo-frontend' to reattach the detached terminals"
